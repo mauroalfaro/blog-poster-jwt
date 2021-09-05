078709f164c338dc7299dfaca717c83ed1a9cb4d
Mauro Alfaro <mauro@appsky.io>
2021-09-05 00:43:11 -0300

    Added User auth with JWT

--- app/api.py
+++ app/api.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI
-from app.model import PostSchema
+from fastapi import FastAPI, Body, Depends
+
+from app.model import PostSchema, UserSchema, UserLoginSchema
+from app.auth.auth_bearer import JWTBearer
+from app.auth.auth_handler import signJWT
+
+
 
 posts = [
     {
@@ -37,10 +42,31 @@ async def lookup_post(id: int) -> dict:
             }
 
 
-@app.post("/posts", tags=["posts"])
+@app.post("/posts", dependencies=[Depends(JWTBearer())], tags=["posts"])
 async def add_post(post: PostSchema) -> dict:
     post.id = len(posts) + 1
     posts.append(post.dict())
     return {
-        "data": "Post added successfully."
+        "data": "post added."
+    }
+
+
+
+@app.post("/user/signup", tags=["user"])
+async def create_user(user: UserSchema = Body(...)):
+    users.append(user) # replace with db call, making sure to hash the password first
+    return signJWT(user.email)
+
+def check_user(data: UserLoginSchema):
+    for user in users:
+        if user.email == data.email and user.password == data.password:
+            return True
+    return False
+
+@app.post("/user/login", tags=["user"])
+async def user_login(user: UserLoginSchema = Body(...)):
+    if check_user(user):
+        return signJWT(user.email)
+    return {
+        "error": "Wrong login details!"
     }
--- app/model.py
+++ app/model.py
@@ -12,3 +12,29 @@ class PostSchema(BaseModel):
                 "content": "This app is secured by authentication using JWT. Uses PyJWT to sign, encode and decode JWT tokens."
             }
         }
+
+class UserSchema(BaseModel):
+    fullname: str = Field(...)
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "fullname": "Mauro Alfaro",
+                "email": "mauro@alfaro.io",
+                "password": "weakpassword"
+            }
+        }
+
+class UserLoginSchema(BaseModel):
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "mauro@alfaro.io",
+                "password": "weakpassword"
+            }
+        }
